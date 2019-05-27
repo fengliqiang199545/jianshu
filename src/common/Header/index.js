07c4e7a03dc9d6a2d988f1a1b992eda71087f672
fengliqiang199545 <fengliqiang199545@gmail.com>
2019-05-27 18:03:04 +0800

    jianshu project

---
+++ src/common/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {};
+
+export default Header;
